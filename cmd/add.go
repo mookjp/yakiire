eea39f950015e4beac4ba80fc9704576bbd43062
Varun Patil <varun@mercari.com>
2019-07-03 15:08:41 +0900

    feat: Implement an add command

---
+++ cmd/add.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/mookjp/yakiire/lib"
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a document to a collection",
+	Long:  `Add a single document to a collection with a random ID`,
+	Run: func(cmd *cobra.Command, args []string) {
+		documentStr := args[0]
+		if documentStr == "" {
+			fmt.Printf("The document you entered seems to be empty!")
+			os.Exit(1)
+		}
+
+		var doc interface{}
+		err := json.Unmarshal([]byte(documentStr), &doc)
+		fmt.Println(doc)
+
+		flags := cmd.Flags()
+		collectionName, err := flags.GetString(cmdCollectionsKey)
+		if err != nil {
+			panic(err)
+		}
+
+		config := getConfig(cmd.Root())
+		cred := config.credentialPath
+		projectId := config.projectId
+
+		ctx := context.Background()
+		client, err := lib.NewClient(ctx, &lib.ClientConfig{
+			Credentials: cred,
+			ProjectID:   projectId,
+		})
+		if err != nil {
+			fmt.Printf("error: %+v", err)
+			os.Exit(1)
+		}
+
+		getCtx, _ := context.WithCancel(ctx)
+		res, err := client.Add(getCtx, collectionName, doc)
+		if err != nil {
+			fmt.Printf("error: %+v", err)
+			os.Exit(1)
+		}
+		ctx.Done()
+
+		fmt.Println("Document addded with ID:", res.ID)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	addCmd.Flags().StringP(cmdCollectionsKey, "c", "", "The collection name to add a document to")
+	err := addCmd.MarkFlagRequired(cmdCollectionsKey)
+	if err != nil {
+		panic(err)
+	}
+}
--- lib/client.go
+++ lib/client.go
@@ -86,3 +86,13 @@ func (c *Client) Query(ctx context.Context, collection string, conditions []*Con
 	}
 	return res, nil
 }
+
+// Add a document into a collection
+func (c *Client) Add(ctx context.Context, collection string, document interface{}) (*firestore.DocumentRef, error) {
+	collectionRef := c.firestore.Collection(collection)
+	res, _, err := collectionRef.Add(ctx, document)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
