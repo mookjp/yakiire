f5a4e5c21a1ec2953da2f8574540b7467d9fd391
Varun Patil <varun@mercari.com>
2019-07-04 17:53:17 +0900

    docs(query): add rudimentary description

--- cmd/query.go
+++ cmd/query.go
@@ -35,13 +35,8 @@ import (
 // queryCmd represents the query command
 var queryCmd = &cobra.Command{
 	Use:   "query",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Query a firestore collection",
+	Long:  `Query a firestore collection`,
 	Run: func(cmd *cobra.Command, args []string) {
 		collectionName := GetFlag(cmd, cmdCollection, true).(string)
 		limit := GetFlag(cmd, cmdLimit, true).(int)
